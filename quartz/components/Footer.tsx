1167e8a2e7e989ffb2f90bf22251a9affde9b455
Kunal Kundu <kunal99kundu@gmail.com>
2024-01-19 21:33:43 +0530

    remove framework name & icon

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
 
 interface Options {
   links: Record<string, string>
@@ -19,9 +18,6 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
-        <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>
-        </p>
       </footer>
     )
   }
