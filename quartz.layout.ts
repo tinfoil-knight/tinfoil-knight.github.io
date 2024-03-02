6b3aa1692c50edeb3b6a658c748b319a3a78e814
Kunal Kundu <kunal99kundu@gmail.com>
2024-03-03 02:08:45 +0530

    disable unused components, prevent widescreen layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,12 +27,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     // Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    // Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    // Component.Graph(),
+    // Component.DesktopOnly(Component.TableOfContents()),
+    // Component.Backlinks(),
   ],
 }
 
